2230426eb9c909b8d8f6fdd39c5a01053757cb4e
Josue Plaza <plazajosue2@gmail.com>
2019-09-27 09:07:08 -0500

    September Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
-require('scrollmagic');
+// require('scrollmagic');
+//
+// var controller = new ScrollMagic.Controller();
+//
+// // create a scene
+// new ScrollMagic.Scene({
+//     duration: 100,    // the scene should last for a scroll distance of 100px
+//     offset: 50    // start this scene after scrolling for 50px
+// })
+//     .setPin("#my-sticky-element") // pins the element for the the scene's duration
+//     .addTo(controller); // assign the scene to the controller
 
-var controller = new ScrollMagic.Controller();
-
-// create a scene
-new ScrollMagic.Scene({
-    duration: 100,    // the scene should last for a scroll distance of 100px
-    offset: 50    // start this scene after scrolling for 50px
-})
-    .setPin("#my-sticky-element") // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
+require('./bootstrap.js');
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link href="https://fonts.googleapis.com/css?family=Coustard|Miriam+Libre:700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
         @yield('title')
     </title>
@@ -16,6 +18,9 @@
 
     @yield('bottomscript')
     <script type="text/javascript" src="/js/skrollr.min.js"></script>
+    <script type="text/javascript" src="/js/TweenMax.min.js"></script>
+    <script type="text/javascript" src="/js/ScrollMagic.min.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
     <script type="text/javascript">
         var s = skrollr.init({
             edgeStrategy: 'set',
@@ -26,5 +31,80 @@
                 }
             }
         });
+
+        let lastScrollNumber = 0;
+        let scrollDirection = '';
+
+        const animateLine = (lineId, left = false) => {
+            var aboutMeLine = document.querySelector(`#${lineId}`);
+            if (aboutMeLine === null) {
+                return;
+            }
+            var aboutMeLineLength = aboutMeLine.getTotalLength();
+
+            // Offsetting Dash apparently...
+            aboutMeLine.style.strokeDasharray = 1000;
+            aboutMeLine.style.strokeDashoffset = 1000;
+
+            window.addEventListener("scroll", (e) => {
+                let calculatedPath = 0;
+                calculatedPath = (left) ? document.documentElement.scrollTop : document.documentElement.scrollTop * -1;
+                let drawLength = calculatedPath;
+
+                    // detect scroll direction b/c js is fucking retarded
+                    if (lastScrollNumber > document.documentElement.scrollTop) {
+                        scrollDirection = 'down';
+                    } else if (lastScrollNumber < document.documentElement.scrollTop) {
+                        scrollDirection = 'up';
+                    }
+                    lastScrollNumber = document.documentElement.scrollTop;
+
+                if (document.documentElement.scrollTop > 195 && document.documentElement.scrollTop < 700) {
+                    if (left) {
+                        if (scrollDirection == 'down') {
+                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) - 10;
+                        } else {
+                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) + 10;
+                        }
+                    } else {
+                        if (scrollDirection == 'down') {
+                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) + 10;
+                        } else {
+                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) - 10;
+                        }
+                    }
+                }
+            });
+        };
+
+        animateLine('about-line-left', true);
+        animateLine('about-line-right');
+
+        $('.carousel').carousel({
+            interval: 2500
+        });
+
+        var scrollMagicController = new ScrollMagic();
+        // var blurbTween = TweenMax.staggerFromTo('#blurb .item', 0.5,
+        //     {
+        //         y: 50,
+        //         x: -50,
+        //         opacity: 0
+        //     },
+        //     {
+        //         y: 0,
+        //         x: 0,
+        //         opacity: 1
+        //     },
+        //     0.2
+        // );
+        // var blurbScene = new ScrollScene({
+        //     triggerElement: '#blurb .item',
+        //     duration: 500,
+        //     offset: -200
+        // })
+        // .setTween(blurbTween)
+        // .addTo(scrollMagicController);
+
     </script>
 </body>

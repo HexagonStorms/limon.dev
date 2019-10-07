4a7cf3000adf7a025d656f3d6d378321938715da
Josue Plaza <plazajosue2@gmail.com>
2019-10-06 20:18:28 -0500

    Finish About section

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,12 +19,16 @@
     @yield('content')
 
     @yield('bottomscript')
+
     <script type="text/javascript" src="/js/skrollr.min.js"></script>
-    <script type="text/javascript" src="/js/animation.gsap.min.js"></script>
-    <script type="text/javascript" src="/js/TweenMax.min.js"></script>
-    {{--<script type="text/javascript" src="/js/ScrollMagic.min.js"></script>--}}
+    <script type="text/javascript" src="/js/TweenMax.js"></script>
+    <script type="text/javascript" src="/js/ScrollMagic.js"></script>
+    <script type="text/javascript" src="/js/plugins/animation.gsap.js"></script>
+    <script type="text/javascript" src="/js/plugins/debug.addIndicators.js"></script>
     <script type="text/javascript" src="/js/app.js"></script>
     <script type="text/javascript">
+
+        // Landscape Parallax Scroll
         var s = skrollr.init({
             edgeStrategy: 'set',
             easing: {
@@ -35,79 +39,29 @@
             }
         });
 
-        let lastScrollNumber = 0;
-        let scrollDirection = '';
-
-        const animateLine = (lineId, left = false) => {
-            var aboutMeLine = document.querySelector(`#${lineId}`);
-            if (aboutMeLine === null) {
-                return;
-            }
-            var aboutMeLineLength = aboutMeLine.getTotalLength();
-
-            // Offsetting Dash apparently...
-            aboutMeLine.style.strokeDasharray = 1000;
-            aboutMeLine.style.strokeDashoffset = 1000;
-
-            window.addEventListener("scroll", (e) => {
-                let calculatedPath = 0;
-                calculatedPath = (left) ? document.documentElement.scrollTop : document.documentElement.scrollTop * -1;
-                let drawLength = calculatedPath;
-
-                    // detect scroll direction b/c js is fucking retarded
-                    if (lastScrollNumber > document.documentElement.scrollTop) {
-                        scrollDirection = 'down';
-                    } else if (lastScrollNumber < document.documentElement.scrollTop) {
-                        scrollDirection = 'up';
-                    }
-                    lastScrollNumber = document.documentElement.scrollTop;
-
-                if (document.documentElement.scrollTop > 195 && document.documentElement.scrollTop < 700) {
-                    if (left) {
-                        if (scrollDirection == 'down') {
-                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) - 10;
-                        } else {
-                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) + 10;
-                        }
-                    } else {
-                        if (scrollDirection == 'down') {
-                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) + 10;
-                        } else {
-                            aboutMeLine.style.strokeDashoffset = parseInt(aboutMeLine.style.strokeDashoffset) - 10;
-                        }
-                    }
-                }
-            });
-        };
-
-        animateLine('about-line-left', true);
-        animateLine('about-line-right');
-
-        $('.carousel').carousel({
-            interval: 2500
+        $( document ).ready(function() {
+            var scrollMagicController = new ScrollMagic.Controller();
+            var blurbTween = TweenMax.staggerFromTo('#blurb .item', 0.5,
+                {
+                    y: 50,
+                    x: -50,
+                    opacity: 0
+                },
+                {
+                    y: 0,
+                    x: 0,
+                    opacity: 1
+                },
+                0.2
+            )
+            var blurbScene = new ScrollMagic.Scene({
+                triggerElement: '#blurb .item',
+                duration: 300,
+                offset: 0
+            })
+            .setTween(blurbTween)
+            .addTo(scrollMagicController);
         });
-
-        var scrollMagicController = new ScrollMagic();
-        var blurbTween = TweenMax.staggerFromTo('#blurb .item', 0.5,
-            {
-                y: 50,
-                x: -50,
-                opacity: 0
-            },
-            {
-                y: 0,
-                x: 0,
-                opacity: 1
-            },
-            0.2
-        );
-        var blurbScene = new ScrollMagic.Scene({
-            triggerElement: '#blurb .item',
-            duration: 500,
-            offset: -200
-        })
-        .setTween(blurbTween)
-        .addTo(scrollMagicController);
 
     </script>
 </body>
